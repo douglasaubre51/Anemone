bfcd5207677fc3ef47ad02ee93d69bfe4b7aa618
Allen Bose <boseallen192@gmail.com>
2026-05-08 22:17:07 +0530

    Update to api url!

--- Anemone/Store/MioApiStore.cs
+++ Anemone/Store/MioApiStore.cs
@@ -2,6 +2,6 @@
 
 public static class MioApiStore
 {
-    //public static string BaseUrl = "https://mioapi.onrender.com/api";
-    public static string BaseUrl = "http://localhost:5182/api";
+    public static string BaseUrl = "https://mioapi.onrender.com/api";
+    //public static string BaseUrl = "http://localhost:5182/api";
 }
